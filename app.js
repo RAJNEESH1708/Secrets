fe5911a601bbcb7dccf80a194fc1a82418c5d1e9
RAJNEESH PANDEY <Rajneesh Pandey>
2021-01-11 16:42:32 +0530

    lev.3 hash

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const md5 = require("md5");
-const encrypt = require("mongoose-encryption")
+// const encrypt = require("mongoose-encryption")
+
 
 const app = express();
 
@@ -51,10 +52,12 @@ const userSchema = new mongoose.Schema({
     },
 });
 
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["password"],
-});
+// userSchema.plugin(encrypt, {
+//   secret: process.env.SECRET,
+//   encryptedFields: ["password"],
+// });
+
+
 
 
 const User = new mongoose.model("User",userSchema)
@@ -73,7 +76,7 @@ app.post('/register', function (req, res) {
     
     const newUser = new User({
         email : req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     }) 
     
     newUser.save(function (err) {
@@ -96,7 +99,7 @@ app.post("/login", function (req, res) {
          {console.log(err);}
          else{
              if(foundUser){
-                 if(foundUser.password === req.body.password)
+                 if(foundUser.password === md5(req.body.password))
                  {
                      res.render("secrets")
                  }
